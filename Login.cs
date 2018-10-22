93f9410f910dfacfd9f00f618e8a875560f14bc2
Vince <github_vjdw@vwingate.sent.com>
2018-10-22 22:57:00 +0100

    Create a JWT

--- Login.cs
+++ Login.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,7 +9,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Collections.Specialized;
+using JWT.Builder;
 
 namespace KnowShow
 {
@@ -23,8 +25,22 @@ namespace KnowShow
             string password = req.Form["password"];
 
             return username != null && password == "hunter2"
-                ? (ActionResult)new OkObjectResult($"Hello, {username}")
+                ? (ActionResult)new OkObjectResult(CreateJwt())
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+        private static string CreateJwt()
+        {
+            var privateKeyPem = Environment.GetEnvironmentVariable("JwtPrivatePem", EnvironmentVariableTarget.Process);
+
+            var token = new JwtBuilder()
+                .WithAlgorithm(new JWT.Algorithms.HMACSHA512Algorithm())
+                .WithSecret(privateKeyPem)
+                .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
+                .AddClaim("claim2", "claim2-value")
+                .Build();
+
+            return token;
         }
     }
 }
